4355cb3f1c4275ef91b15c4b7ef6cfaaf339c759
Mathias Larsen <mathiaslarsen1122@gmail.com>
2025-01-30 12:29:41 +0100

    niv1 03 done

---
+++ Opgaver/Niveau1/03/script.js
@@ -0,0 +1,18 @@
+
+function skjulElementViaCssSelector(cssSelector){
+    let elements = document.querySelectorAll(cssSelector)
+    elements.forEach(function(element){
+        
+        skjulElement(element)
+
+    })
+
+    
+}
+
+function skjulElement (element){
+    if(element !== null) element.style.display = "none"
+}
+
+
+skjulElementViaCssSelector(".bodycopy")
