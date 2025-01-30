dbbf51c52d932e2d30ca3015fe7f2e16f900fed0
Mathias Larsen <mathiaslarsen1122@gmail.com>
2025-01-30 11:26:03 +0100

    niv 1 02 done

--- Opgaver/Niveau1/02/script.js
+++ Opgaver/Niveau1/02/script.js
@@ -4,3 +4,19 @@ function beregnForhold ( a, b ){
 
 let resultat = beregnForhold(4,3)
 console.log(resultat);
+
+
+function beregnForhold ( a, b ){
+    
+    if(b == 0) return null
+
+
+    return a / b
+}
+
+let resultat2 = beregnForhold(12,0)
+console.log(resultat2);
+
+
+
+
